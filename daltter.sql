81dd9807e24bb60716bd46cc8eb730449e61ea9e
qualitypaper <115874127+qualitypaper@users.noreply.github.com>
2023-04-24 18:32:07 +0200

    update of database

--- daltter.sql
+++ daltter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 05, 2023 at 11:32 PM
+-- Generation Time: Apr 24, 2023 at 07:31 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.9
 
@@ -85,16 +85,19 @@ CREATE TABLE `subscription` (
   `created_at` timestamp NOT NULL,
   `days_to_expire` int DEFAULT NULL,
   `site_name` varchar(45) DEFAULT NULL,
-  `url` text,
+  `url` varchar(128) DEFAULT NULL,
   `started` tinyint NOT NULL DEFAULT '0',
-  `country_tags` text,
-  `theme_tags` text,
+  `country_tags` varchar(64) DEFAULT NULL,
+  `theme_tags` varchar(64) DEFAULT NULL,
   `amount_of_messages_per_day` int NOT NULL,
   `user_id` int NOT NULL,
-  `mode` varchar(2) DEFAULT NULL,
+  `mode` varchar(4) DEFAULT NULL,
   `paused` tinyint DEFAULT '0',
   `expired` tinyint NOT NULL DEFAULT '0',
-  `statistics_id` int DEFAULT NULL
+  `statistics_id` int DEFAULT NULL,
+  `sub_duration` int DEFAULT '14',
+  `price_per_message` int NOT NULL DEFAULT '0',
+  `amount_of_messages_already_sent` int DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -114,6 +117,23 @@ CREATE TABLE `subscription_statistics` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `subscription_view`
+-- (See below for the actual view)
+--
+CREATE TABLE `subscription_view` (
+`sub_id` int
+,`country_tags` varchar(64)
+,`price_per_message` int
+,`pricing` int
+,`user_id` int
+,`email` varchar(32)
+,`formatted_url` varchar(64)
+,`url` varchar(32)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sub_orders`
 --
 
@@ -123,7 +143,8 @@ CREATE TABLE `sub_orders` (
   `title` text,
   `emails_to` text,
   `amount_of_messages` int NOT NULL,
-  `sub_id` int NOT NULL
+  `sub_id` int NOT NULL,
+  `daily_updated` tinyint DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -140,7 +161,8 @@ CREATE TABLE `user_info` (
   `created` timestamp NOT NULL,
   `confirmed` tinyint NOT NULL DEFAULT '0',
   `role` varchar(45) NOT NULL DEFAULT 'ROLE_USER',
-  `two_step` tinyint NOT NULL DEFAULT '0'
+  `two_step` tinyint NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -156,6 +178,15 @@ CREATE TABLE `verification_token` (
   `sended_at` timestamp NOT NULL,
   `confirmed_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `subscription_view`
+--
+DROP TABLE IF EXISTS `subscription_view`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `subscription_view`  AS SELECT `s`.`id` AS `sub_id`, `s`.`country_tags` AS `country_tags`, `s`.`price_per_message` AS `price_per_message`, `s`.`pricing` AS `pricing`, `ui`.`id` AS `user_id`, `ui`.`email` AS `email`, `ss`.`formatted_url` AS `formatted_url`, `ss`.`url` AS `url` FROM ((`subscription` `s` left join `subscription_statistics` `ss` on((`s`.`statistics_id` = `ss`.`id`))) left join `user_info` `ui` on((`ui`.`id` = `s`.`user_id`)))  ;
 
 --
 -- Indexes for dumped tables
@@ -189,13 +220,16 @@ ALTER TABLE `statistics`
 -- Indexes for table `subscription`
 --
 ALTER TABLE `subscription`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subscription_id_idx` (`id`),
+  ADD UNIQUE KEY `subscription_idx` (`id`,`theme_tags`,`country_tags`,`days_to_expire`,`url`,`created_at`,`statistics_id`);
 
 --
 -- Indexes for table `subscription_statistics`
 --
 ALTER TABLE `subscription_statistics`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subscription_statistics_idx` (`id`,`url`,`date`,`visits`,`formatted_url`);
 
 --
 -- Indexes for table `sub_orders`
@@ -208,7 +242,8 @@ ALTER TABLE `sub_orders`
 --
 ALTER TABLE `user_info`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email_UNIQUE` (`email`);
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD UNIQUE KEY `user_info_idx` (`id`,`full_name`,`email`,`password`,`created`,`confirmed`,`role`,`two_step`);
 
 --
 -- Indexes for table `verification_token`
